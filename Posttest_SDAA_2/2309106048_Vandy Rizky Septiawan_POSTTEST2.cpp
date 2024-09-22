5197424806b629c09dcba90d6f57b9a7e3d53368
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2024-09-22 08:18:07 +0800

    eror bagian edit aksesoris

--- Posttest_SDAA_2/2309106048_Vandy Rizky Septiawan_POSTTEST2.cpp
+++ Posttest_SDAA_2/2309106048_Vandy Rizky Septiawan_POSTTEST2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 struct kamera{
     string namaKamera;
     string harga;
+    string aksesoris[3];
+    int jumlahAcc = 0;
+    int maxAcc = sizeof(aksesoris)/sizeof(aksesoris[0]);
 };
 
 string menu(){
@@ -15,14 +18,87 @@ string menu(){
 	"5. Keluar\n";
 }
 
+void tambahAksesoris(kamera arr[], int *idx){
+    cout << "Masukkan nama aksesoris >> ";
+    cin.ignore();
+    getline(cin, arr[*idx].aksesoris[arr[*idx].jumlahAcc]);
+    arr[*idx].jumlahAcc++;
+}
+
+void tampilkanAksesoris(kamera arr[], int idx){
+    for (int i = 0; i < arr[idx].jumlahAcc; i++){
+        cout << "\tAksesoris ke-" << i+1 << " " << arr[idx].aksesoris[i] << endl;
+    }    
+}
+
+void editAksesoris(kamera arr[], int idx){
+    cout << "1. Ganti nama aksesoris\n"
+    "2. Hapus aksesoris\n";
+    string pilih;
+    cout << "Masukkan pilihan >> ";
+    cin >> pilih;
+    if (pilih == "1"){
+        for (int i = 0; i < arr[idx].jumlahAcc; i++){
+            cout << i+1 << ".\tAksesoris: " << arr[idx].aksesoris[i] << endl;;
+        }
+        int idxAcc;
+        cout << "Masukkan indeks aksesoris >> ";
+        cin >> idxAcc;
+        if (idxAcc > arr[idx].jumlahAcc){
+            cout << "Indeks aksesoris tidak ditemukan, pengeditan aksesoris gagal" << endl;
+        }else{
+            cout << "Masukkan nama aksesoris >> ";
+            cin.ignore();
+            getline(cin, arr[idx].aksesoris[idxAcc-1]);
+        }
+    }
+    //error
+    // else if (pilih == "2"){
+    //     for (int i = 0; i < arr[idx].jumlahAcc; i++){
+    //         cout << i+1 << ".\tAksesoris: " << arr[idx].aksesoris[i] << endl;;
+    //     }
+    //     int idxAcc;
+    //     cout << "Masukkan indeks aksesoris >> ";
+    //     cin >> idxAcc;
+    //     if (idxAcc > arr[idx].jumlahAcc){
+    //         cout << "Indeks aksesoris tidak ditemukan, penghapusan aksesoris gagal" << endl;
+    //     }else{
+    //         for (int i = idxAcc-1; i < arr[idx].maxAcc; i++){
+    //             arr[idx].aksesoris[i] = arr[idx].aksesoris[i+1];
+    //         }
+    //         arr[idx].jumlahAcc--;
+    //     }
+    // }
+
+    
+    else{
+        cout << "Pilihan tidak ada!!!" << endl;
+    }
+    
+
+}
+
 void tambah(kamera arr[], int& jml, int kapasitas){
     if (jml == kapasitas){
         cout << "Tidak dapat menambahkan data kamera" << endl;
     }else{
         cout << "Masukkan nama kamera >> ";
+        cin.ignore();
         getline(cin, arr[jml].namaKamera);
         cout << "Masukkan harga >> ";
         getline(cin, arr[jml].harga);
+        string pilih;
+        cout << "Apakah ingin menambahkan aksesoris >> (y/t)";
+        cin >> pilih;
+        while (pilih == "y"){
+            tambahAksesoris(arr, &jml);
+            if(arr[jml].jumlahAcc < arr[jml].maxAcc){
+                cout << "Apakah ingin menambahkan aksesoris (y/t) >> ";
+                cin >> pilih;
+            }else{
+                break;
+            }
+        }
         jml++;
     }
 }
@@ -34,6 +110,10 @@ void lihat(kamera arr[], int& jml, int kapasitas){
         for (int i = 0; i < jml; i++){
             cout << i+1 << ".\tNama kamera: " << arr[i].namaKamera << endl;
             cout << "\tHarga: " << arr[i].harga << endl;
+            if (arr[i].jumlahAcc > 0){
+                tampilkanAksesoris(arr, i);
+                cout << endl;
+            }
             cout << endl;
         }
     }    
@@ -47,7 +127,7 @@ void hapus (kamera arr[], int& jml, int kapasitas){
         int idx;
         cout << "Masukkan indeks yang ingin dihapus >> ";
         cin >> idx;
-        if (idx > jml){
+        if (idx > jml || idx <= 0){
             cout << "Indeks kamera tidak mencapai "<< idx<< endl;
         }else{
             for (int i = idx-1; i < jml; i++){
@@ -59,7 +139,7 @@ void hapus (kamera arr[], int& jml, int kapasitas){
     }
 }
 
-void edit (kamera arr[], int& jml, int kapasitas){ //belum jadi
+void edit (kamera arr[], int& jml, int kapasitas){
     if (jml == 0){
         cout << "Belum ada data kamera" << endl;
     }else{
@@ -67,8 +147,27 @@ void edit (kamera arr[], int& jml, int kapasitas){ //belum jadi
         int idx;
         cout << "Masukkan indeks yang ingin diedit >> ";
         cin >> idx;
-        if (idx > jml){
+        if (idx > jml || idx <= 0){
             cout << "Indeks kamera tidak mencapai "<< idx<< endl;
+        }else{
+            cout << "Nama kamera\t: " << arr[idx-1].namaKamera << endl;
+            cout << "Harga\t\t: " << arr[idx-1].harga << endl;
+            if (arr[idx-1].jumlahAcc > 0){
+                tampilkanAksesoris(arr, idx);
+            }
+            cout << "Masukkan nama kamera baru >> ";
+            cin.ignore();
+            getline(cin, arr[idx-1].namaKamera);
+            cout << "Masukkan harga kamera baru >> ";
+            getline(cin, arr[idx-1].harga);
+            string edit;
+            if (arr[idx-1].jumlahAcc > 0){
+                cout << "Ingin edit aksesoris (y/t) >> ";
+                cin >> edit;
+                if (edit == "y"){
+                    editAksesoris(arr, idx-1);
+                }
+            }
         }
     }
 
@@ -81,12 +180,13 @@ void tampilan(kamera arr[], int& jml, int kapasitas){
         cout << "Masukkan pilihan >> ";
         cin >> pilih;
         if (pilih == "1"){
-            cin.ignore();
             tambah(arr, jml, kapasitas);
         }else if (pilih == "2"){
             lihat(arr, jml, kapasitas);
         }else if(pilih == "3"){
             hapus(arr, jml, kapasitas);
+        }else if(pilih == "4"){
+            edit(arr, jml, kapasitas);
         }else if(pilih == "5"){
             ulang = "t";
         }else{
